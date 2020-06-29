18bdd4e12f8b3a707e67118ae587e9da437e4aba
孙凯 <sunkai60@jd.com>
2020-06-29 10:08:32 +0800

    增加测试loader

--- my-webpack/loaders/less-loader.js
+++ my-webpack/loaders/less-loader.js
@@ -1,7 +1,11 @@
 let lessLoader = require('less')
+let loaderUtils = require('loader-utils')   // 获取当前loader中的配置
 
 function lessFn (source) {
     let css
+
+    // console.log(this)
+    // console.log(loaderUtils.getOptions(this))
 
     lessLoader.render(source, (err, code) => {
         css = code.css.replace(/\n+/g, '\\n')
